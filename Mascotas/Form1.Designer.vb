e0f04249080ff687cc0d235d917f33c00f308a7f
Matias192020 <elnegrocap44@gmail.com>
2020-05-13 19:30:15 -0300

    Agregar archivos de proyecto.

---
+++ Mascotas/Form1.Designer.vb
@@ -0,0 +1,203 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnagregar = New System.Windows.Forms.Button()
+        Me.txtcolor = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtraza = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtnombre = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btneliminar = New System.Windows.Forms.Button()
+        Me.btneditar = New System.Windows.Forms.Button()
+        Me.btnsalir = New System.Windows.Forms.Button()
+        Me.grdmascotas = New System.Windows.Forms.DataGridView()
+        Me.txtid = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.grdmascotas, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtid)
+        Me.GroupBox1.Controls.Add(Me.btneliminar)
+        Me.GroupBox1.Controls.Add(Me.btnsalir)
+        Me.GroupBox1.Controls.Add(Me.btnagregar)
+        Me.GroupBox1.Controls.Add(Me.btneditar)
+        Me.GroupBox1.Controls.Add(Me.txtcolor)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtraza)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtnombre)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(112, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(323, 382)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Datos Mascotas"
+        '
+        'btnagregar
+        '
+        Me.btnagregar.Location = New System.Drawing.Point(209, 78)
+        Me.btnagregar.Name = "btnagregar"
+        Me.btnagregar.Size = New System.Drawing.Size(75, 23)
+        Me.btnagregar.TabIndex = 5
+        Me.btnagregar.Text = "Agregar"
+        Me.btnagregar.UseVisualStyleBackColor = True
+        '
+        'txtcolor
+        '
+        Me.txtcolor.Location = New System.Drawing.Point(84, 114)
+        Me.txtcolor.Name = "txtcolor"
+        Me.txtcolor.Size = New System.Drawing.Size(100, 20)
+        Me.txtcolor.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(29, 114)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Color:"
+        '
+        'txtraza
+        '
+        Me.txtraza.Location = New System.Drawing.Point(84, 78)
+        Me.txtraza.Name = "txtraza"
+        Me.txtraza.Size = New System.Drawing.Size(100, 20)
+        Me.txtraza.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Raza:"
+        '
+        'txtnombre
+        '
+        Me.txtnombre.Location = New System.Drawing.Point(84, 35)
+        Me.txtnombre.Name = "txtnombre"
+        Me.txtnombre.Size = New System.Drawing.Size(100, 20)
+        Me.txtnombre.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(23, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre:"
+        '
+        'btneliminar
+        '
+        Me.btneliminar.Location = New System.Drawing.Point(149, 286)
+        Me.btneliminar.Name = "btneliminar"
+        Me.btneliminar.Size = New System.Drawing.Size(75, 24)
+        Me.btneliminar.TabIndex = 3
+        Me.btneliminar.Text = "Eliminar"
+        Me.btneliminar.UseVisualStyleBackColor = True
+        '
+        'btneditar
+        '
+        Me.btneditar.Location = New System.Drawing.Point(60, 286)
+        Me.btneditar.Name = "btneditar"
+        Me.btneditar.Size = New System.Drawing.Size(66, 24)
+        Me.btneditar.TabIndex = 2
+        Me.btneditar.Text = "Editar"
+        Me.btneditar.UseVisualStyleBackColor = True
+        '
+        'btnsalir
+        '
+        Me.btnsalir.Location = New System.Drawing.Point(109, 316)
+        Me.btnsalir.Name = "btnsalir"
+        Me.btnsalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnsalir.TabIndex = 2
+        Me.btnsalir.Text = "Salir"
+        Me.btnsalir.UseVisualStyleBackColor = True
+        '
+        'grdmascotas
+        '
+        Me.grdmascotas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdmascotas.Location = New System.Drawing.Point(479, 13)
+        Me.grdmascotas.Name = "grdmascotas"
+        Me.grdmascotas.Size = New System.Drawing.Size(309, 413)
+        Me.grdmascotas.TabIndex = 3
+        '
+        'txtid
+        '
+        Me.txtid.Location = New System.Drawing.Point(95, 250)
+        Me.txtid.Name = "txtid"
+        Me.txtid.Size = New System.Drawing.Size(100, 20)
+        Me.txtid.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(102, 225)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(82, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Editar o Eliminar"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.CadetBlue
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.grdmascotas)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Mascotas"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.grdmascotas, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnagregar As Button
+    Friend WithEvents txtcolor As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtraza As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtnombre As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btneliminar As Button
+    Friend WithEvents btneditar As Button
+    Friend WithEvents btnsalir As Button
+    Friend WithEvents grdmascotas As DataGridView
+    Friend WithEvents txtid As TextBox
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Mascotas/Form1.vb
@@ -0,0 +1,175 @@
+﻿Imports MySql.Data.MySqlClient
+
+
+
+Public Class Form1
+
+    Sub ActualizarSelect()
+
+        Dim ds As DataSet = New DataSet
+        Dim adaptador As MySqlDataAdapter = New MySqlDataAdapter
+
+        Dim conexion As MySqlConnection = New MySqlConnection
+        Dim cmd As New MySqlCommand
+
+        conexion.ConnectionString = "server=localhost; database=veterinaria;Uid=root;Pwd=;"
+
+        Try
+            conexion.Open()
+            'MsgBox("nos conectamos")
+            cmd.Connection = conexion
+
+            cmd.CommandText = "SELECT * FROM perros ORDER BY id ASC"
+            adaptador.SelectCommand = cmd
+            adaptador.Fill(ds, "Tabla")
+            grdmascotas.DataSource = ds
+            grdmascotas.DataMember = "Tabla"
+
+            conexion.Close()
+
+        Catch ex As Exception
+            MsgBox("No ha conectado con la base de Datos.")
+        End Try
+
+
+    End Sub
+    Private Sub btnsalir_Click(sender As Object, e As EventArgs) Handles btnsalir.Click
+        End
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ActualizarSelect()
+
+    End Sub
+
+    Private Sub btnagregar_Click(sender As Object, e As EventArgs) Handles btnagregar.Click
+        Dim ds As DataSet = New DataSet
+        Dim adaptador As MySqlDataAdapter = New MySqlDataAdapter
+        Dim conexion As MySqlConnection = New MySqlConnection
+        Dim cmd As New MySqlCommand
+        conexion.ConnectionString = "server=localhost; database=veterinaria;Uid=root;Pwd=;"
+
+        Try
+
+            conexion.Open()
+            'MsgBox("Nos conectamos")
+            cmd.Connection = conexion
+            cmd.CommandText = "INSERT INTO perros(nombremascota,raza,color) VALUES(@nombremascota,@raza,@color)"
+            cmd.Prepare()
+            cmd.Parameters.Clear()
+            cmd.Parameters.AddWithValue("@nombremascota", txtnombre.Text)
+            cmd.Parameters.AddWithValue("@raza", txtraza.Text)
+            cmd.Parameters.AddWithValue("@color", txtcolor.Text)
+            cmd.ExecuteNonQuery()
+            txtnombre.Clear()
+            txtraza.Clear()
+            txtcolor.Clear()
+            conexion.Close()
+
+
+
+            'MsgBox("Su mascota a sido ingresada correctamente")
+            ActualizarSelect()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+
+        End Try
+    End Sub
+
+    Private Sub grdmascotas_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles grdmascotas.CellContentClick
+        If (grdmascotas.SelectedRows.Count > 0) Then
+            txtnombre.Text = grdmascotas.Item("nombre", grdmascotas.SelectedRows(0).Index).Value
+            txtraza.Text = grdmascotas.Item("raza", grdmascotas.SelectedRows(0).Index).Value
+            txtcolor.Text = grdmascotas.Item("color", grdmascotas.SelectedRows(0).Index).Value
+            txtid.Text = grdmascotas.Item("id", grdmascotas.SelectedRows(0).Index).Value
+
+
+
+        End If
+
+    End Sub
+
+    Private Sub btneditar_Click(sender As Object, e As EventArgs) Handles btneditar.Click
+        Dim ds As DataSet = New DataSet
+        Dim adaptador As MySqlDataAdapter = New MySqlDataAdapter
+        Dim conexion As MySqlConnection = New MySqlConnection
+        Dim cmd As New MySqlCommand
+        conexion.ConnectionString = "server=localhost; database=veterinaria;Uid=root;Pwd=;"
+        Try
+            conexion.Open()
+
+            cmd.Connection = conexion
+            cmd.CommandText = "UPDATE perros SET nombremascota=@nombremascota, raza=@raza, color=@color WHERE id=@id"
+            cmd.Prepare()
+            cmd.Parameters.Clear()
+            cmd.Parameters.AddWithValue("@nombremascota", txtnombre.Text)
+            cmd.Parameters.AddWithValue("@raza", txtraza.Text)
+            cmd.Parameters.AddWithValue("@color", txtcolor.Text)
+            cmd.Parameters.AddWithValue("@id", txtid.Text)
+            cmd.ExecuteNonQuery()
+            txtnombre.Clear()
+            txtraza.Clear()
+            txtcolor.Clear()
+            txtid.Clear()
+            conexion.Close()
+
+            'MsgBox("Su mascota a sido editada correctamente")
+            ActualizarSelect()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+
+        End Try
+    End Sub
+
+    Private Sub btneliminar_Click(sender As Object, e As EventArgs) Handles btneliminar.Click
+        Dim ds As DataSet = New DataSet
+        Dim adaptador As MySqlDataAdapter = New MySqlDataAdapter
+        Dim conexion As MySqlConnection = New MySqlConnection
+        Dim cmd As New MySqlCommand
+        conexion.ConnectionString = "server=localhost; database=veterinaria;Uid=root;Pwd=;"
+        Try
+            conexion.Open()
+            'MsgBox("Nos conectamos")
+            cmd.Connection = conexion
+            cmd.CommandText = "DELETE perros WHERE nombremascota=@nombremascota AND raza=@raza AND color=@color AND id=@id"
+            cmd.Prepare()
+            cmd.Parameters.Clear()
+            cmd.Parameters.AddWithValue("@nombremascota", txtnombre.Text)
+            cmd.Parameters.AddWithValue("@raza", txtraza.Text)
+            cmd.Parameters.AddWithValue("@color", txtcolor.Text)
+            cmd.Parameters.AddWithValue("@id", txtid.Text)
+            cmd.ExecuteNonQuery()
+            txtnombre.Clear()
+            txtraza.Clear()
+            txtcolor.Clear()
+            txtid.Clear()
+            conexion.Close()
+
+            'MsgBox("Su mascota a sido eliminada correctamente")
+            ActualizarSelect()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+
+        End Try
+    End Sub
+
+    Private Sub txtid_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub GroupBox2_Enter(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub txtid_TextChanged_1(sender As Object, e As EventArgs) Handles txtid.TextChanged
+
+    End Sub
+End Class
